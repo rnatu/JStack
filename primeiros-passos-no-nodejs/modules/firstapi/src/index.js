8f95bbb800e9196a50a33e1dc12a5717d05a9f82
Renato Xavier <rnatu91@gmail.com>
2023-11-01 10:44:56 -0300

    feat: add query params

--- primeiros-passos-no-nodejs/modules/firstapi/src/index.js
+++ primeiros-passos-no-nodejs/modules/firstapi/src/index.js
@@ -1,19 +1,26 @@
 const http = require("http");
 const routes = require("./routes");
+const { URL } = require("url");
 
 const server = http.createServer((request, response) => {
-  console.log(`Request method: ${request.method} | Endpoint: ${request.url}`);
+  const parsedUrl = new URL(`http://localhost:3000${request.url}`);
+
+  // console.log(
+  //   `Request method: ${request.method} | Endpoint: ${parsedUrl.pathname}`
+  // );
 
   const route = routes.find(
     (routeObj) =>
-      routeObj.endpoint === request.url && routeObj.method === request.method
+      routeObj.endpoint === parsedUrl.pathname &&
+      routeObj.method === request.method
   );
 
   if (route) {
+    request.query = Object.fromEntries(parsedUrl.searchParams);
     route.handler(request, response);
   } else {
     response.writeHead(404, { "Content-Type": "text/html" });
-    response.end(`Cannot ${request.method} ${request.url}`);
+    response.end(`Cannot ${request.method} ${parsedUrl.pathname}`);
   }
 });
 
