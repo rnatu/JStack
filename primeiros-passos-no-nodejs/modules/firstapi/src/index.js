304469170c4e3348db1ff055f25d732908a6619c
Renato Xavier <rnatu91@gmail.com>
2023-11-08 10:31:55 -0300

    feat: add params url and filter users by

--- primeiros-passos-no-nodejs/modules/firstapi/src/index.js
+++ primeiros-passos-no-nodejs/modules/firstapi/src/index.js
@@ -9,14 +9,25 @@ const server = http.createServer((request, response) => {
   //   `Request method: ${request.method} | Endpoint: ${parsedUrl.pathname}`
   // );
 
+  let { pathname } = parsedUrl;
+  let id = null;
+
+  const splitEndpoint = pathname.split("/").filter(Boolean);
+
+  if (splitEndpoint.length > 1) {
+    pathname = `/${splitEndpoint[0]}/:id`;
+    id = splitEndpoint[1];
+  }
+
   const route = routes.find(
     (routeObj) =>
-      routeObj.endpoint === parsedUrl.pathname &&
-      routeObj.method === request.method
+      routeObj.endpoint === pathname && routeObj.method === request.method
   );
 
   if (route) {
     request.query = Object.fromEntries(parsedUrl.searchParams);
+    request.params = { id };
+
     route.handler(request, response);
   } else {
     response.writeHead(404, { "Content-Type": "text/html" });
