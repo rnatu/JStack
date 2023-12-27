91e39fa3b458ed995dcb1dd58e27334fcd199e49
Renato <rnatu91@gmail.com>
2023-12-27 10:46:47 -0300

    feat: add query to create table contacts on postgres docker

--- nodejs-com-express/mycontacts/src/database/schema.sql
+++ nodejs-com-express/mycontacts/src/database/schema.sql
@@ -1,9 +1,23 @@
+-- \l (listando databases)
 CREATE DATABASE mycontacts;
 
+-- \c mycontacts (selecionando database)
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE
   IF NOT EXISTS categories (
     id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4 (),
-    name VARCHAR(255) NOT NULL
+    name VARCHAR NOT NULL
   );
+
+CREATE TABLE
+  IF NOT EXISTS contacts (
+    id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4 (),
+    name VARCHAR NOT NULL,
+    email VARCHAR UNIQUE,
+    phone VARCHAR,
+    category_id UUID,
+    FOREIGN KEY (category_id) REFERENCES categories (id)
+  );
+
+-- \dt (listando tabelas)
