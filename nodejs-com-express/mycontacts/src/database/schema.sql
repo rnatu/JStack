fe3852113f2c4e86cd1fefa0c979a95a592db413
Renato Xavier <rnatu91@gmail.com>
2024-04-22 11:09:00 -0300

    Refactor: adjust identation on schema.sql

--- nodejs-com-express/mycontacts/src/database/schema.sql
+++ nodejs-com-express/mycontacts/src/database/schema.sql
@@ -4,15 +4,13 @@ CREATE DATABASE mycontacts;
 -- \c mycontacts (selecionando database)
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE
-  IF NOT EXISTS categories (
-    id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4 (),
+CREATE TABLE IF NOT EXISTS categories (
+    id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
     name VARCHAR NOT NULL
-  );
+);
 
-CREATE TABLE
-  IF NOT EXISTS contacts (
-    id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4 (),
+CREATE TABLE IF NOT EXISTS contacts (
+    id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
     name VARCHAR NOT NULL,
     email VARCHAR UNIQUE,
     phone VARCHAR,
@@ -21,3 +19,7 @@ CREATE TABLE
   );
 
 -- \dt (listando tabelas)
+
+-- Exibir conteúdo da tabela categories por exemplo:
+SELECT * FROM categories;
+
