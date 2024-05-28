6b091f86bc68cfa0a751355316772b1b0311cf77
Renato Xavier <rnatu91@gmail.com>
2024-05-28 11:36:33 -0300

    feat: add error handler midleware from express

--- nodejs-com-express/mycontacts/src/index.js
+++ nodejs-com-express/mycontacts/src/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+require('express-async-errors');
+
 const routes = require('./routes');
 
 const app = express();
@@ -6,5 +8,14 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
+
+// error handlers precisam vir depois das rotas
+// precisam da lib express-async-errors para tratar erros assíncronos
+// eslint-disable-next-line no-unused-vars
+app.use((error, request, response, next) => {
+  console.log('Error handler #################################');
+  console.log(error);
+  response.sendStatus(500);
+});
 
 app.listen(3000, () => console.log('🔥 Server started at http://localhost:3000'));
